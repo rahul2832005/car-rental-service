cb6bd6a8c64167fa4e6834b508bb705a9e5905a1
B M VATUKIYA <133945333+bhupatcode@users.noreply.github.com>
2025-01-01 08:51:31 +0530

    Update login.php
    add php

--- user/login.php
+++ user/login.php
@@ -1,4 +1,76 @@
+<?php
+//login page
+    ob_start();
+    session_start();
+    error_reporting(0);
+    $conn=mysqli_connect("localhost","root","","car_rent");
+    if(!$conn)
+    {
+        echo "Not connect";
+    }
 
+    if(isset($_POST["login"]))
+    {
+        $count=0;
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $query="select * from reguser where email='$email' && password='$password'";
+        $exquery=mysqli_query($conn,$query);
+
+        $row=mysqli_num_rows($exquery);
+
+        if($row==1)
+        {
+            $show="select * from reguser where email='$email'";
+            $data=mysqli_query($conn,$show);
+            $user=mysqli_fetch_assoc($data);
+            // $id = $user["id"];
+            // $_SESSION["userid"]=$id;
+            
+
+            $username=$user["name"];
+            $_SESSION["alogin"]=$username;
+           
+             header("location:dis_car.php");
+
+        }
+        else {
+        ?>
+            <script type="text/javascript">
+            alert("NO User Found Of  This Details !");</script>
+
+        <?php  }
+        if($email=="")
+        {
+            $em="Enter Email ID !";
+            $count++;
+        }
+        else
+        {
+            $ex1='/^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-z]/';
+            if(!preg_match($ex1,$email))
+            {
+                $em="Enter Valid Email Address !";
+                $count++;
+            }
+        }
+        if($password=="")
+        {
+            $pass="Enter The Password";
+            $count++;
+        }
+        else
+        {
+            if(strlen($password)<8)
+            {
+                $pass="Enter At leasr 8 character !";
+                $count++;
+            }
+        }
+        
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,6 +109,7 @@
             border-radius: 10px;
             padding: 30px 40px;
             backdrop-filter: blur(6px);
+          
         }
 
         .container h1 {
@@ -170,24 +243,25 @@
             <h1>Login</h1>
 
             <div class="input-box">
-                <input type="text" placeholder="Email" name="email"/>
+                <input type="text" placeholder="Email" name="email" value="<?php echo $email; ?>"/>
+                <p style="color: red;"><?php echo $em; ?></p>
                 
 
             </div>
 
             <div class="input-box">
-                <input type="password" placeholder="Password" name="password"/>
-                
+                <input type="password" placeholder="Password" name="password" value="<?php echo $password; ?>"/>
+                <p style="color: red;"><?php echo $pass; ?></p>
             </div>
 
             <div class="remember-forgot">
                 <label><input type="checkbox" /> Remember me</label>
                 <a href="#"> Forgot Password?</a>
             </div>
-            <button type="submit" class="btn" name="submit">Log in</button>
+            <button type="submit" class="btn" name="login">Log in</button>
 
             <div class="register-link">
-                <p>Don't have an account?<a href="register1.php">Register Here!</a></p>
+                <p>Don't have an account?<a href="register.php">Register Here!</a></p>
             </div>
         </form>
     </div>
